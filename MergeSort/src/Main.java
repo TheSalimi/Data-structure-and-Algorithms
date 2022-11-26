2d46d55f1b1a01f7d366879a41c1f1d94e04b7d3
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-11-27 03:27:31 +0330

    Merge Sort with java

---
+++ MergeSort/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+
+        Sort(arr, 0, arr.length - 1);
+
+        for (int number : arr) System.out.print(number + " ");
+    }
+
+    static void Sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            Sort(arr, left, middle);
+            Sort(arr, middle + 1, right);
+
+            Merge(arr, left, middle, right);
+        }
+    }
+
+    static void Merge(int[] arr, int left, int middle, int right) {
+        int numberOfLeftElements = middle - left + 1;
+        int numberOfRightElements = right - middle;
+
+        int[] leftSubTree = new int[numberOfLeftElements];
+        int[] rightSubTree = new int[numberOfRightElements];
+
+        for (int i = 0; i < leftSubTree.length; ++i) leftSubTree[i] = arr[i + left];
+
+        for (int i = 0; i < rightSubTree.length; ++i) rightSubTree[i] = arr[i + middle + 1];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < numberOfLeftElements && j < numberOfRightElements) {
+            if (leftSubTree[i] <= rightSubTree[j]) {
+                arr[k] = leftSubTree[i];
+                i++;
+            } else {
+                arr[k] = rightSubTree[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < numberOfLeftElements) {
+            arr[k] = leftSubTree[i];
+            i++;
+            k++;
+        }
+
+        while (j < numberOfRightElements) {
+            arr[k] = rightSubTree[j];
+            j++;
+            k++;
+        }
+    }
+}
