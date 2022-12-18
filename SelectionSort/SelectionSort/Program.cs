e9ca5c3d9eb18e1f8f68a9385c8a90356a9bb8b2
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-12-18 17:10:26 +0330

    Selection Sort with C#

---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void SelectionSort(int [] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = i;
+
+                for (int j = i+1; j < arr.Length; j++)
+                    if (arr[j] < arr[min])
+                        min = j;
+
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
