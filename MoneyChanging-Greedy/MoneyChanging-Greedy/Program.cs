b6959cab3a1c5b7a853a39cd4ca1dba4ac570a7b
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-12-26 21:21:55 +0330

    MoneyChanging
    using greedy algorithm

---
+++ MoneyChanging-Greedy/MoneyChanging-Greedy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyChanging_Greedy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (m > 0)
+            {
+                if (m >= 10)
+                {
+                    count++;
+                    m -= 10;
+                }
+                else if (m < 10 && m >= 5)
+                {
+                    count++;
+                    m -= 5;
+                }
+                else
+                {
+                    count++;
+                    m -= 1;
+                }
+            }
+            Console.Write(count);
+            Console.ReadKey();
+        }
+    }
+}
