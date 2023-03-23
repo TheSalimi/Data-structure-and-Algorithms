3929d6bb608c193bc461fca745d5b14136bc51e1
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2023-03-23 16:48:49 +0430

    Create shortestPath.cs
    گرافی n راسی ھمبند بدون جھت با 1 - n یال داریم. حال بھ شما دو راس u و v داده میشود و شما
    باید طبق الگوریتم DFS فاصلھ (تعداد حداقل یال) برای رسیدن از راس u بھ v را بگویید.
    
    ورودی:
    ابتدا عدد n داده میشود کھ 100000 <= n <= 1
    سپس در 1 – n خط بعدی ۲ عدد داده میشود کھ بھ ترتیب ۲راس یک یال ھستند
    سپس ۲ راس u و vداده میشود
    خروجی:
    فاصلھ دو راس u و v را چاپ نمایید

---
+++ DFS/shortestPath.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HW3_1
+{
+    class Program
+    {
+        static bool exit = false;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int>[] adj = new List<int>[n+1];
+
+            for (int i = 0; i <= n; i++)
+                adj[i] = new List<int>();
+            
+            for (int i = 0; i < n-1; i++)
+            {
+                int[] a_b =Array.ConvertAll( Console.ReadLine().Split(), int.Parse);
+                addEdge(a_b[0], a_b[1], adj);
+            }
+            int[] u_v = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            DFS(u_v[0], -1, 0, u_v[1],adj);
+        }
+
+        static void addEdge(int a , int b , List<int>[]adj)
+        {
+            adj[a].Add(b);
+            adj[b].Add(a);
+        }
+
+        static void DFS(int node,int parent,int level,int dest, List<int>[]adj)
+        {
+            if (node != dest)
+            {
+                foreach (int child in adj[node])
+                {
+                    if (child != parent)
+                    {
+                        DFS(child, node, level + 1, dest, adj);
+                        if (exit)
+                            return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(level);
+                exit = true;
+            }
+        }
+    }
+}
