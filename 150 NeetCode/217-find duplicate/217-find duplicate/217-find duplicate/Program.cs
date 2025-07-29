473858368ecfecbec6baaa48379734a5962d7d93
Amir Salimi <amirhosein.srudsari@gmail.com>
2025-07-29 21:47:06 +0330

    217 - duplicate

---
+++ 150 NeetCode/217-find duplicate/217-find duplicate/217-find duplicate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _217_find_duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = [1,2,4,5,6,7,8,1];
+            HashSet<int> set = new HashSet<int>();
+            
+            foreach(int a in arr)
+            {
+                if (set.Contains(a))
+                {
+                    Console.WriteLine($"duplicate : {a}");
+                    break;
+                }
+                set.Add(a);
+            }
+        }
+    }
+}
